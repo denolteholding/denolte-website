b7dfb2a1566f297f05c64ff1b3c9903d79c9a437
noltedennis <dennis_nolte@gmx.de>
2020-11-28 21:19:45 +0100

    Add -n flag for git clone

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git config --global push.default simple
 # Current dir is build root (with "build" directory containg the fresh build)
 
 # Prepare deployment directory (separate git repo)
-git clone -b master https://github.com/denolteholding/denolteholding.github.io.git deployment
+git clone -n -b master https://github.com/denolteholding/denolteholding.github.io.git deployment
 
 # Copy all files from build into the deployment directory
 ls -l deployment
@@ -19,12 +19,8 @@ rsync -av --exclude ".git" ./build/ deployment
 
 # Perform GIT push ("|| true" for no content changes)
 cd deployment
-ls -l
-git status
 git add -A
-git status
 git commit -m "Rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
-git status
 git push
 cd ..
 
